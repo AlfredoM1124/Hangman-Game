316ec2c15d9ec6598d98ac4e2d92b4df222afa40
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-08-14 17:37:34 -0400

    Updated working homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,107 @@
 
-		// VARIABLES
-		// =================================
-		// The Words that will be randomly cycled through for the user to answer.
-		var words = ["Jetpack","Gorilla","Mongolian","Historical","Theater","Festival","Partying","Glasses",
-				 	 "Touchdown","Canal","Tabletop","Military","Jackpot","Scuba",""];
+/* game.js javascript file */
 
-		var randomWord = []
-		// The available options for user to guess.
-		var ltr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-		// Start the game with 0 wins.
-		var wins = 0;
-		// Start the game with 0 losses.
-		var losses = 0;
-		// User gets 8 guesses before receiving a loss.
-		var guessesIndex = 0;
+var wordsArray = ["Jetpack","Gorilla","Mongolian","Historical","Theater","Festival","Partying","Glasses",
+				"Touchdown","Canal","Tabletop","Military","Jackpot","Scuba","Hydroelectricity","Words","Keys"];
+var currentWord = [];
+var totalWords = wordsArray.length;
+var guessesRemaining = 9;
+var wins = 0;
+var losses = 0;
+var Correctguesses = 0;
+var prevGuess= [];
+// Render a random word to begin the game - - 
+var randomWord = document.getElementById("current-word");
 
-		var guessesRemaining = 9;
-		var guessesMade = [];
-		
+randomWord.innerHTML = "";
 
-		// FUNCTIONS 
-		// =================================
-
-		// Function to update the scores
-	function updateScore(){
-		var guessesLeft = guessesRemaining - guessIndex;
-		document.querySelector("#wins").innerHTML = "Wins: " + wins;
-		document.querySelector("losses").innerHTML = "Losses: " + losses;
-		document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-	}
-
-		
-	function challenge(){
-		rand = Math.floor(Math.random()*challenges.length);
-		word = challenges[rand];
-		numChar = 1;
-		var letter = word.substring(0,1);
-			document.getElementById('letter1').innerHTML = letter;
-			document.getElementById('letter1').style.visibility = "hidden";
-			document.getElementById('underline1').style.borderBottom = "3px solid black";
-			document.getElementById('underline1').style.display = "block";
-			document.getElementById('gamePage').style.display = "block";
-			document.getElementById('categoryName').style.width = "100%";
-	}
-	// Main Process
-
-	// Calls the function to set the scores and guesses to 0
-	updateScore();
-
-	// Record the user's guesses
-
-	document.onkeyup = function(event){
-		// Determine which key was pressed 
-		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-		// For loop to determine how many guesses the user has left. 
-		for (var inputGuess = 0; inputGuess < guessesRemaining; inputGuess++){
-			// If user guesses correctly keep the same amount of guesses remaining and fill a blank.
-			if (userInput === ){
-
-			}
-			// If they guess wrong reduce guesses remaining by 1
-			else{
-				guessesLeft--;
-				guessesMade.push(userInput);
-				showGuesses();
-				updateScore();
-			}
-		}
-	}
+for(var i=0;i<totalWords;i++){  
+    randomWord.innerHTML += "_"; 
+    currentWord.push("_");
 }
-		
+// - - Random Word generator ends
+var currentProgress = randomWord.innerHTML;
+
+// Keep track of player's keyboard inputs
+ document.onkeyup = function(event) {
+     var attempts = event.key;    
+     // Only run if user has guesses remaining 
+     if(guessesRemaining > 0){   
+          // Continue if the user's guess has not been chosen yet 
+        if(prevGuess.indexOf(attempts) < 0){
+            
+              console.log("User guessed: " + attempts);
+            
+             if((wordsArray.indexOf(attempts) >= 0)){
+                 var currentGuess = "";
+                 console.log("Alert Alert Alert Alert");
+                 for(var i=0 ; i < wordsArray.length; i++){
+                     if(wordsArray.charAt(i) === attempts){
+                         console.log("Found " + attempts + " at index: " + i);
+                         Correctguesses++;
+                         currentWord[i] = attempts;
+                         
+                         console.log("Progress: " + currentWord);
+                     }
+                 }
+                 currentWord.forEach(function(thing){
+                    currentGuess += thing;
+                 });
+                     
+                 randomWord.innerHTML = currentGuess;
+                 
+             }
+             else{
+                 
+                guessesRemaining--;
+                document.getElementById("guesses-left").innerHTML = guessesRemaining;
+                 
+                 if(guessesRemaining<9 && guessesRemaining>=5){
+                    document.getElementById("guesses-left").style.color = "#008000";
+                 }
+                 else if(guessesRemaining<5 && guessesRemaining>=2){
+                    document.getElementById("guesses-left").style.color = "#ff6600";
+                 }
+                 else if(guessesRemaining<2){
+                    document.getElementById("guesses-left").style.color = "#ff0000";
+                 }
+             }
+            
+            prevGuess.push(attempts);
+            document.getElementById("used-letters").innerHTML = prevGuess;
+            console.log("Guess List: " + prevGuess + "\nGuesses Left: " + guessesRemaining + "\nCorrect Guesses: ");
+        }
+         
+         if(Correctguesses == totalWords){
+             wins++; document.getElementById("wins").innerHTML = wins;
+             resetValues();
+         }
+     
+ }
+     else{
+         losses++;
+         document.getElementById("losses").innerHTML = losses;
+         resetValues();
+     }
+     
+ };
+
+// Resets the game statistics and words
+function resetValues(){
+    guessesLeft = 9;
+    Correctguesses = 0;
+    guessList = [];
+    currentWord = [];
+    
+    randomWord.innerHTML = "";
+    document.getElementById("guesses-left").innerHTML = guessesRemaining;
+    document.getElementById("used-letters").innerHTML = "";
+    document.getElementById("guesses-left").style.color = "black";
+    
+    
+    for(var i=0;i<totalWords;i++){  
+        randomWord.innerHTML += "_"; 
+        currentWord.push("_");
+    }
+    currentProgress = randomWord.innerHTML;
+}
